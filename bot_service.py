77147d30226025c755e71cc5de4ebed669a45dd0
Shazy <shoplik.konstantin@yahoo.com>
2023-10-14 15:18:41 +0300

    added autoreset colorama style in console output

--- bot_service.py
+++ bot_service.py
@@ -1,19 +1,21 @@
 import os
 import requests
-from colorama import Fore
+from colorama import init, Fore
 from dotenv import load_dotenv
+
 
 class BotService:
     def __init__(self):
         load_dotenv()
+        init(autoreset=True)
         try:
             self.BOT_TOKEN = os.getenv("BOT_TOKEN")
             if self.validate_token(self.BOT_TOKEN):
-                print(Fore.GREEN, f"{self.bot_username} has successfully connected to telegram")
+                print(Fore.GREEN + f"{self.bot_username} has successfully connected to telegram")
             else:
-                print(Fore.RED, 'Invalid bot token')
+                print(Fore.RED + 'Invalid bot token')
         except:
-            print(Fore.RED, 'please add your telegram bot token in the env file')
+            print(Fore.RED + 'please add your telegram bot token in the env file')
             exit
 
     def validate_token(self, bot_token):
