a39a3461d690808b146ccc3c0cc50eb257fac197
Shazy <shoplik.konstantin@yahoo.com>
2023-12-15 16:21:19 +0300

    #8 - Creating SparkSession on bot startup

--- create_bot.py
+++ create_bot.py
@@ -1,7 +1,9 @@
 from aiogram import Bot, Dispatcher
 import bot_service
+from database import postgesql
 
 service = bot_service.BotService()
+spark = postgesql.PostgreDB()
 
 bot: Bot = Bot(token=service.BOT_TOKEN, parse_mode='HTML')
 dp: Dispatcher = Dispatcher()
